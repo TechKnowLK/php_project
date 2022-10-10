60ce68a0cae6dc526d802334da2a2fe73beec539
TechKnowLK <samarasingha2004@gmail.com>
2022-10-10 06:33:37 -0700

    PHP Project

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="plugin/bootstrap.min.css">
+	<script src="plugin/jquery.min.js"></script>
+	<script src="plugin/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+	<title>King Amda</title>
+</head>
+<body>
+
+	<?php include_once('inc/navbar.php') ?>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="jumbotron mt-4">
+					<h4 id="jumbo-header">Welcome To The King Amda</h4>
+					<h4 id="jumbo-emoji">👾🦾👨‍💻</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
